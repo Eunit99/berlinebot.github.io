200f58fa74e42d960d50d28d362f214048b6a98f
Berlinebot <44534317+Berlinebot@users.noreply.github.com>
2018-11-04 19:13:30 +0100

    Update berline_contact.js

--- js/berline_contact.js
+++ js/berline_contact.js
@@ -1,8 +1,88 @@
-  // <![CDATA[ 
-   $(document).ready(function() { 
-      $("#chat-container").click(function() { 
-        $("#about-container").css("display", "none");
-      }); 
-   }); 
+   /*
+=================================
+*Dialogue confirmation for OzyPeace.com
+*Copyright: EunitDesigns.com
+*Script developed by Eunit
+=================================
+
+=========DO NOT MODIFY===========
+
+*/
+     
+	       // <![CDATA[ 
+		$(document).ready(function(){
+		    $("#chat").click(function(){
+		        $("#about-container").hide();   
+		    				$("#chat-container").show();
+		    	 });
+		  $("#about").click(function(){
+		        $("#chat-container").hide();
+		 							$("#about-container").show();
+	     });
+		});
+
    // ]]> 
-   
+     
+            //<![CDATA[
+            function Twitter()
+            {
+            //When button is clicked, alert that...
+            alert(""); return false;
+            }
+            //]]>
+            //<![CDATA[
+            function Pinterest()
+            {
+            //When button is clicked, alert that...
+
+            alert(""); return false;
+            }
+            //]]>
+            //<![CDATA[
+            function Instagram()
+            {
+            //When button is clicked, alert that...
+
+            alert(""); return false;
+            }
+            //]]>
+            //<![CDATA[
+            function Googleplus()
+            {
+            //When button is clicked, alert that...
+
+            alert(""); return false;
+            }
+             //<![CDATA[
+            function GetInTouch()
+            {
+            //When button is clicked, alert that...
+
+            alert("Use the social media accounts to connect with me"); return false;
+            }
+            //]]>
+           //<![CDATA[
+            function mail(){
+   var confS = confirm("You are about to mail my developer, do you wish to continue?");
+          if (confS == true){ 
+          alert("Here you go 😃");
+          window.location = "mailto:eunithelp@gmail.com";          return true;
+        } else {
+        
+          return false;
+        }
+    }
+     //]]>
+      //<![CDATA[
+            function Call(){
+   var confC = confirm("Do you really want to call me?");
+          if (confC == true){ 
+          window.location = "tel:+2348110863115";          return true;
+        } else {
+        
+          return false;
+        }
+    }
+           
+            //]]>
+  
